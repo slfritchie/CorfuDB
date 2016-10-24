932190bca1a1e3a283307d86b6fef7cab2a2da88
Scott Lystig Fritchie <sfritchie@vmware.com>
2016-10-24 16:35:25 -0400

    Clarification comment in Rank.java

--- src/main/java/org/corfudb/infrastructure/Rank.java
+++ src/main/java/org/corfudb/infrastructure/Rank.java
@@ -35,6 +35,17 @@ public class Rank implements Comparable<Rank> {
         ** with a duplicate rank would sometime succeed.  Here is why.
         ** TODO: uncomment this code once its purpose is understood.
         ** The LayoutServerTest.checkPhase1AndPhase2MessagesFromMultipleClients test is broken until this hack is reverted.
+        **
+        ** OK, I now understand what's happening here.  The commented code below
+        ** is acting as a tie-breaker that traditional Paxos doesn't have.  Paxos
+        ** relies the uniqueness of the rank integer alone, which means that each
+        ** client needs to use only a subset of all possible ranks, e.g. reserve some
+        ** lower bits for a client-ID-like-unique-across-all-participants integer.
+        **
+        ** This hidden comparison to the clientId string does NOT play nicely with
+        ** QuickCheck: with the clientId hidden, QC has no way to tell why a server
+        ** decided to compare rank (rank = integer only) in crazy and
+        ** nondeterministic ways.
 
         if (rank.compareTo(other.getRank()) == 0) {
             return clientId.compareTo(other.clientId);
