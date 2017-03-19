4268170b85cf3f6fd2a4a5d7cac55e876e5dec6a
Scott Lystig Fritchie <slfritchie@snookles.com>
2017-03-19 15:45:43 -0400

    WIP: cooperative scheduler to assist exposing thread races & lock abuse

---
+++ runtime/src/main/java/org/corfudb/util/CoopScheduler.java
@@ -0,0 +1,407 @@
+package org.corfudb.util;
+
+import org.apache.commons.lang.math.RandomUtils;
+
+import java.util.*;
+
+/** A cooperative scheduler framework for deterministic thread
+ *  scheduling (or as deterministic-as-feasible).
+ *
+ *  A typical workflow for threads that wish to use this
+ *  scheduler will go through these stages:
+ *
+ *  1. A coordinating thread calls reset(Max) to clear the central
+ *     scheduler's state, where Max is the limit # of threads that
+ *     can the central scheduler may manage.
+ *
+ *  2. One thread (typically the coordinator) sets the execution
+ *     schedule with the central scheduler via setSchedule().  A
+ *     schedule is an array integers or list of CoopQuantum where
+ *     the next thread to be scheduled is chosen round-robin from
+ *     the schedule array/list.  An item in the schedule is
+ *     ignored if the thread ID chosen has not yet registered,
+ *     if that thread has exited the scheduler via threadDone(),
+ *     or if the thread is blocking on an operation that cannot
+ *     be managed by this scheduler (as indicated by calling
+ *     the withdraw() function).
+ *
+ *     See makeSchedule() for one method for generating a
+ *     schedule.
+ *
+ *  3. All participating threads indicate their participation
+ *     with a call to registerThread().
+ *     Each thread is assigned a unique thread ID integer, up
+ *     to the limit set by the reset(Max) call.
+ *     If the new thread ID does not appear in the schedule,
+ *     the ID will be added.  However, only a single entry
+ *     will be added: if the schedule is a long list, then the
+ *     result will be infrequent scheduling.
+ *
+ *  4. Whenever a thread wishes to permit the central scheduler
+ *     to make a scheduling change, it calls sched().
+ *
+ *  5. All participating threads must eventually call
+ *     threadDone().
+ *
+ *  6. Meanwhile, one thread (typically the coordinator) starts
+ *     scheduling assignments by calling runScheduler().  This
+ *     function returns when all threads have called
+ *     threadDone().
+ */
+
+public class CoopScheduler {
+    private static class CoopThreadStatus {
+        boolean ready = false;
+        boolean done = false;
+        Integer ticks = 0;
+    };
+
+    public static class CoopQuantum {
+        int thread;
+        int ticks;
+
+        public CoopQuantum(int thread, int ticks) {
+            this.thread = thread;
+            this.ticks = ticks;
+        }
+    };
+
+    static int maxThreads = -1;
+    static int numThreads;
+    static public ThreadLocal<Integer> threadMap;
+    static HashSet<Integer> threadSet;
+    static boolean centralStopped;
+    static final Object centralReady = new Object();
+    static CoopThreadStatus threadStatus[];
+    static CoopQuantum schedule[];
+
+    static List log;
+
+    public static void reset(int maxT) {
+        maxThreads = maxT;
+        numThreads = 0;
+        threadMap = ThreadLocal.withInitial(() -> -1);
+        threadSet = new HashSet<>();
+        centralStopped = false;
+        threadStatus = new CoopThreadStatus[maxThreads];
+        for (int t = 0; t < maxThreads; t++) {
+            threadStatus[t] = new CoopThreadStatus();
+        }
+        schedule = null;
+        log = Collections.synchronizedList(new ArrayList<Object>());
+    }
+
+    public static int registerThread() {
+        return registerThread(-1);
+    }
+
+    public static int registerThread(int t) {
+        synchronized (centralReady) {
+            if (centralStopped == true) {
+                System.err.printf("Central scheduler has stopped scheduling, TODO\n");
+                return -1;
+            }
+            if (threadMap.get() >= 0) {
+                System.err.printf("Thread already registered, TODO\n");
+                return -1;
+            }
+            if (t < 0) {
+                t = numThreads++;
+            } else {
+                // Caller asked for a specific number.  Let's avoid duplicates.
+                if (threadSet.contains(t)) {
+                    System.err.printf("Thread id %d already in use, TODO\n", t);
+                    return -1;
+                }
+                if (t >= numThreads) {
+                    numThreads = t + 1;
+                }
+            }
+            if (t >= maxThreads) {
+                System.err.printf("Too many threads, TODO\n");
+                return -1;
+            }
+            threadMap.set(t);
+            threadSet.add(t);
+            // Don't set ready[t] here.  Instead, do it near top of sched().
+            return t;
+        }
+    }
+
+    public static void threadDone() {
+        threadDone(threadMap.get());
+    }
+
+    public static void threadDone(int t) {
+        try {
+            threadStatus[t].done = true;
+            synchronized (threadStatus[t]) {
+                threadStatus[t].notify();
+            }
+        } catch (Exception e) {
+            System.err.printf("ERROR: threadDone() exception %s\n", e.toString());
+            return;
+        }
+    }
+
+    public static void setSchedule(int schedIn[]) {
+        schedule = new CoopQuantum[schedIn.length];
+        for (int i = 0; i < schedIn.length; i++) {
+            schedule[i] = new CoopQuantum(schedIn[i], 1);
+        }
+        addMissingThreadsToSchedule();
+    }
+
+    public static void setSchedule(CoopQuantum[] schedIn) {
+        schedule = schedIn;
+        addMissingThreadsToSchedule();
+    }
+
+    public static void setSchedule(List<CoopQuantum> schedIn) {
+        schedule = schedIn.toArray(new CoopQuantum[0]);
+        addMissingThreadsToSchedule();
+    }
+
+    private static void addMissingThreadsToSchedule() {
+        HashSet<Integer> present = new HashSet<>();
+        ArrayList<Integer> missing = new ArrayList<>();
+        int numMissing = 0;
+
+        for (int i = 0; i < schedule.length; i++) {
+            present.add(schedule[i].thread);
+        }
+        for (int i = 0; i < maxThreads; i++) {
+            if (! present.contains(i)) {
+                missing.add(i);
+                numMissing++;
+            }
+        }
+        missing.sort(Comparator.naturalOrder());
+
+        CoopQuantum sched[] = Arrays.copyOf(schedule, schedule.length + numMissing);
+        for (int i = 0; i < numMissing; i++) {
+            sched[schedule.length + i] = new CoopQuantum(missing.get(i), 1);
+        }
+        schedule = sched;
+    }
+
+    private static int schedErrors = 0;
+    public static void sched() {
+        try {
+            sched(threadMap.get());
+        } catch (Exception e) {
+            if (schedErrors++ < 3) {
+                System.err.printf("ERROR: sched() exception %s\n", e.toString());
+            } else if (schedErrors < 5) {
+                System.err.printf("sched() exception warnings suppressed\n");
+            }
+            return;
+        }
+    }
+
+    public static void sched(int t) {
+        // if (t >= 0) { System.err.printf("s%d,\n", t); }
+        if (threadStatus[t].done) {
+            System.err.printf("ERROR: thread has called threadDone()!\n");
+            return;
+        }
+        try {
+            synchronized (threadStatus[t]) {
+                if (threadStatus[t].ticks == 0) {
+                    // If this is our first call ticks sched() after registerThread(), do not check tick balance.
+                } else {
+                    threadStatus[t].ticks--;
+                    if (threadStatus[t].ticks > 0) {
+                        // We still have a tick remaining, don't pester scheduler yet.
+                        return;
+                    }
+                }
+                threadStatus[t].ready = true;
+                threadStatus[t].notify();
+            }
+            synchronized (threadStatus[t]) {
+                while (!centralStopped && threadStatus[t].ticks == 0) {
+                    threadStatus[t].wait();
+                }
+                if (centralStopped) {
+                    System.err.printf("NOTICE scheduler stopped\n"); return;
+                }
+            }
+        } catch (InterruptedException e) {
+            System.err.printf("sched nterrupted, TODO?\n");
+            sched(t);
+        }
+    }
+
+    public static void sched(boolean useCoopSched) {
+        if (useCoopSched) {
+            sched();
+        }
+    }
+
+    public static void withdraw() {
+        try {
+            withdraw(threadMap.get());
+        } catch (Exception e) {
+            if (schedErrors++ < 3) {
+                System.err.printf("ERROR: withdraw() exception %s\n", e.toString());
+            } else if (schedErrors < 5) {
+                System.err.printf("withdraw() exception warnings suppressed\n");
+            }
+            return;
+        }
+    }
+
+    public static void withdraw(int t) throws Exception {
+        if (t < 0 || t >= maxThreads) {
+            throw new Exception("Bogus thread number " + t);
+        }
+        synchronized (threadStatus[t]) {
+            threadStatus[t].ticks = 0;
+            threadStatus[t].ready = false;
+            threadStatus[t].notify();
+        }
+    }
+
+    public static void rejoin() {
+        try {
+            rejoin(threadMap.get());
+        } catch (Exception e) {
+            if (schedErrors++ < 3) {
+                System.err.printf("ERROR: rejoin() exception %s\n", e.toString());
+            } else if (schedErrors < 5) {
+                System.err.printf("rejoin() exception warnings suppressed\n");
+            }
+            return;
+        }
+    }
+
+    public static void rejoin(int t) throws Exception {
+        if (t < 0 || t >= maxThreads) {
+            throw new Exception("Bogus thread number " + t);
+        }
+        threadStatus[t].ready = true;
+    }
+
+    public static void runScheduler(int numStartingThreads) {
+        int t;
+        int given = 0;
+
+        while (! someReady(numStartingThreads)) {
+            try { Thread.sleep(1); } catch (Exception e) {}
+        }
+        // System.err.printf("SCHED: entering main loop\n");
+        while (true) {
+            if (allDone()) {
+                synchronized (centralReady) {
+                    if (allDone()) {
+                        centralStopped = true;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < schedule.length; i++) {
+                t = schedule[i].thread;
+                if (! threadStatus[t].ready || threadStatus[t].done) { continue; }
+                if (schedule[i].ticks < 1) {
+                    System.err.printf("TODO FIX ME Bad ticks value in schedule item %d: %d ticks\n", i, schedule[i].ticks);
+                    return;
+                }
+                synchronized (threadStatus[t]) {
+                    while (threadStatus[t].ticks != 0) {
+                        try {threadStatus[t].wait();} catch (InterruptedException e) { System.err.printf("TODO BUMMER FIX ME\n"); return; }
+                    }
+                    threadStatus[t].ticks = schedule[i].ticks;
+                    threadStatus[t].notify();
+                    given++;
+                }
+
+                synchronized (threadStatus[t]) {
+                    while (!threadStatus[t].done && threadStatus[t].ticks != 0) {
+                        try {threadStatus[t].wait();} catch (InterruptedException e) { System.err.printf("TODO BUMMER FIX ME\n"); return; }
+                    }
+                }
+            }
+        }
+        // System.err.printf("GIVEN = %d,", given);
+    }
+
+    private static boolean someReady(int maxThr) {
+        for (int t = 0; t < maxThr; t++) {
+            if (! threadStatus[t].ready) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean allDone() {
+        for (int t = 0; t < numThreads; t++) {
+            if (threadStatus[t].ready && ! threadStatus[t].done) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void appendLog(Object s) {
+        synchronized (log) {
+            log.add(s);
+        }
+    }
+
+    public static Object[] getLog() {
+        synchronized (log) {
+            return log.toArray();
+        }
+    }
+
+    public static boolean logsAreIdentical(ArrayList<Object[]> logs) {
+        for (int i = 0; i < logs.size() - 2; i++) {
+            Object[] l1 = logs.get(i);
+            Object[] l2 = logs.get(i+1);
+            if (l1.length != l2.length) { return false; }
+            for (int j = 0; j < l1.length; j++) {
+                if (! l1[j].equals(l2[j])) { return false; }
+            }
+        }
+        return true;
+    }
+
+    public static int[] makeSchedule(int maxThreads, int length) {
+        int[] schedule = new int[length];
+        final int winnerThisTime = 10;
+        boolean winnerPossible = RandomUtils.nextInt(100) < winnerThisTime;
+        final int winnerProb = 10;
+        final int winnerMaxLen = 40;
+
+        for (int i = 0; i < schedule.length; i++) {
+            // Sometimes create an unfair schedule for a lucky winner thread.
+            if (winnerPossible && RandomUtils.nextInt(winnerProb) == 0) {
+                int winner = RandomUtils.nextInt(maxThreads);
+                int repeats = RandomUtils.nextInt(winnerMaxLen);
+                while (i < schedule.length && repeats-- > 0) {
+                    schedule[i++] = winner;
+                }
+            } else {
+                schedule[i] = RandomUtils.nextInt(maxThreads);
+            }
+        }
+        return schedule;
+    }
+
+    public static String formatSchedule(int[] schedule) {
+        String s = new String("{");
+        for (int i = 0; i < schedule.length; i++) {
+            s += ((i == 0) ? "" : ",") + schedule[i];
+        }
+        s += "}";
+        return s;
+    }
+
+    public static void printSchedule(int[] schedule) {
+        System.err.printf("schedule = %s\n", formatSchedule(schedule));
+    }
+
+
+}
--- test/src/test/java/org/corfudb/runtime/concurrent/StreamSeekAtomicityTest.java
+++ test/src/test/java/org/corfudb/runtime/concurrent/StreamSeekAtomicityTest.java
@@ -6,10 +6,15 @@ import org.corfudb.runtime.collections.SMRMap;
 import org.corfudb.runtime.object.transactions.AbstractTransactionsTest;
 import org.junit.Assert;
 import org.junit.Test;
+import org.corfudb.util.CoopScheduler;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.corfudb.util.CoopScheduler.*;
 
 /**
  * Created by dmalkhi on 3/17/17.
@@ -19,9 +24,8 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
     @Override
     public void TXBegin() { OptimisticTXBegin(); }
 
-
-
-    protected int numIterations = PARAMETERS.NUM_ITERATIONS_LOW;
+    protected int numIterations = PARAMETERS.NUM_ITERATIONS_MODERATE;
+    private final int innerIterations = 10;
 
     /**
      * This test verifies commit atomicity against concurrent -read- activity,
@@ -31,11 +35,28 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
      */
     @Test
     public void ckCommitAtomicity() throws Exception {
+        ckCommitAtomicity(false);
+    }
+
+    /**
+     * This test verifies commit atomicity against concurrent -read- activity,
+     * which constantly causes rollbacks and optimistic-rollbacks.
+     * Use the cooperative scheduler for thread interleaving control.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ckCommitAtomicityCoop() throws Exception {
+        ckCommitAtomicity(true);
+    }
+
+    public void ckCommitAtomicity(boolean useCoopSched) throws Exception {
         String mapName1 = "testMapA";
         Map<Long, Long> testMap1 = instantiateCorfuObject(SMRMap.class, mapName1);
         CountDownLatch l1 = new CountDownLatch(2);
         AtomicBoolean commitDone = new AtomicBoolean(false);
         final int NTHREADS = 3;
+        int[] schedule = makeSchedule(NTHREADS, numIterations / innerIterations);
 
         scheduleConcurrently(t -> {
 
@@ -46,46 +67,61 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
                 // on first iteration, wait for all to start;
                 // other iterations will proceed right away
                 l1.await();
+                if (txCnt == 0) { coopRegisterThread(useCoopSched, 0); }
 
                 // generate optimistic mutation
+                sched(useCoopSched);
                 testMap1.put(1L, (long)txCnt);
 
                 // wait for it to be undon
+                sched(useCoopSched);
                 TXEnd();
             }
             // signal done
             commitDone.set(true);
 
-            Assert.assertEquals((long)(txCnt-1), (long) testMap1.get(1L));
+            String failMsg = useCoopSched ? ("schedule was: " + formatSchedule(schedule) + "\n") : "";
+            Assert.assertEquals(failMsg, (long)(txCnt-1), (long) testMap1.get(1L));
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps affecting optimistic-rollback of the above thread
         scheduleConcurrently(t -> {
+
             TXBegin();
             testMap1.get(1L);
 
             // signal that transaction has started and obtained a snapshot
             l1.countDown();
+            coopRegisterThread(useCoopSched, 1);
 
             // keep accessing the snapshot, causing optimistic rollback
 
             while (!commitDone.get()){
+                sched(useCoopSched);
                 testMap1.get(1L);
             }
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps syncing with the tail of log
         scheduleConcurrently(t -> {
+
             // signal that thread has started
             l1.countDown();
+            coopRegisterThread(useCoopSched, 2);
 
             // keep updating the in-memory proxy from the log
             while (!commitDone.get() ){
+                sched(useCoopSched);
                 testMap1.get(1L);
             }
+            coopThreadDone(useCoopSched);
         });
 
+        Thread coop = coopSetScheduleAndRun(useCoopSched, NTHREADS, NTHREADS, schedule);
         executeScheduled(NTHREADS, PARAMETERS.TIMEOUT_NORMAL);
+        coopJoinScheduler(coop);
     }
 
     /**
@@ -97,6 +133,23 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
      */
     @Test
     public void ckCommitAtomicity2() throws Exception {
+        ckCommitAtomicity2(false);
+    }
+
+    /**
+     * This test is similar to above, but with multiple maps.
+     * It verifies commit atomicity against concurrent -read- activity,
+     * which constantly causes rollbacks and optimistic-rollbacks.
+     * Use the cooperative scheduler for thread interleaving control.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ckCommitAtomicity2Coop() throws Exception {
+        ckCommitAtomicity2(true);
+    }
+
+    public void ckCommitAtomicity2(boolean useCoopSched) throws Exception {
         String mapName1 = "testMapA";
         Map<Long, Long> testMap1 = instantiateCorfuObject(SMRMap.class, mapName1);
         String mapName2 = "testMapB";
@@ -105,6 +158,7 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
         CountDownLatch l1 = new CountDownLatch(2);
         AtomicBoolean commitDone = new AtomicBoolean(false);
         final int NTHREADS = 3;
+        int[] schedule = makeSchedule(NTHREADS, numIterations / innerIterations);
 
         scheduleConcurrently(t -> {
 
@@ -115,20 +169,27 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
                 // on first iteration, wait for all to start;
                 // other iterations will proceed right away
                 l1.await();
+                if (txCnt == 0) { coopRegisterThread(useCoopSched, 0); }
 
                 // generate optimistic mutation
+                sched(useCoopSched);
                 testMap1.put(1L, (long)txCnt);
-                if (txCnt % 2 == 0)
-                    testMap2.put(1L, (long)txCnt);
+                if (txCnt % 2 == 0) {
+                    sched(useCoopSched);
+                    testMap2.put(1L, (long) txCnt);
+                }
 
-                // wait for it to be undon
+                // wait for it to be undone
+                sched(useCoopSched);
                 TXEnd();
             }
             // signal done
             commitDone.set(true);
 
-            Assert.assertEquals((long)(txCnt-1), (long) testMap1.get(1L));
-            Assert.assertEquals((long)( (txCnt-1) % 2 == 0 ? (txCnt-1) : txCnt-2), (long) testMap2.get(1L));
+            String failMsg = useCoopSched ? ("schedule was: " + formatSchedule(schedule) + "\n") : "";
+            Assert.assertEquals(failMsg, (long)(txCnt-1), (long) testMap1.get(1L));
+            Assert.assertEquals(failMsg, (long)( (txCnt-1) % 2 == 0 ? (txCnt-1) : txCnt-2), (long) testMap2.get(1L));
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps affecting optimistic-rollback of the above thread
@@ -138,28 +199,38 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
 
             // signal that transaction has started and obtained a snapshot
             l1.countDown();
+            coopRegisterThread(useCoopSched, 1);
 
             // keep accessing the snapshot, causing optimistic rollback
 
             while (!commitDone.get()){
+                sched(useCoopSched);
                 testMap1.get(1L);
+                sched(useCoopSched);
                 testMap2.get(1L);
             }
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps syncing with the tail of log
         scheduleConcurrently(t -> {
             // signal that thread has started
             l1.countDown();
+            coopRegisterThread(useCoopSched, 2);
 
             // keep updating the in-memory proxy from the log
             while (!commitDone.get() ){
+                sched(useCoopSched);
                 testMap1.get(1L);
+                sched(useCoopSched);
                 testMap2.get(1L);
             }
+            coopThreadDone(useCoopSched);
         });
 
+        Thread coop = coopSetScheduleAndRun(useCoopSched, NTHREADS, NTHREADS, schedule);
         executeScheduled(NTHREADS, PARAMETERS.TIMEOUT_NORMAL);
+        coopJoinScheduler(coop);
     }
 
     /**
@@ -169,38 +240,64 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
      */
     @Test
     public void ckCommitAtomicity3() throws Exception {
-        String mapName1 = "testMapA";
+        for (int i = 0; i < innerIterations; i++) {
+            ckCommitAtomicity3(i, false);
+        }
+    }
+
+    /**
+     * This test is similar to above, but with concurrent -write- activity.
+     * Use the cooperative scheduler for thread interleaving control.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ckCommitAtomicity3Coop() throws Exception {
+        for (int i = 0; i < innerIterations; i++) {
+            ckCommitAtomicity3(i, true);
+        }
+    }
+
+    public void ckCommitAtomicity3(int iter, boolean useCoopSched) throws Exception {
+        String mapName1 = "testMapA" + iter;
         Map<Long, Long> testMap1 = instantiateCorfuObject(SMRMap.class, mapName1);
-        String mapName2 = "testMapB";
+        String mapName2 = "testMapB" + iter;
         Map<Long, Long> testMap2 = instantiateCorfuObject(SMRMap.class, mapName2);
 
         CountDownLatch l1 = new CountDownLatch(2);
         AtomicBoolean commitDone = new AtomicBoolean(false);
         final int NTHREADS = 3;
+        int[] schedule = makeSchedule(NTHREADS, numIterations / innerIterations);
 
         scheduleConcurrently(t -> {
-
             int txCnt;
-            for (txCnt = 0; txCnt < numIterations; txCnt++) {
+            for (txCnt = 0; txCnt < numIterations / innerIterations; txCnt++) {
                 TXBegin();
 
                 // on first iteration, wait for all to start;
                 // other iterations will proceed right away
                 l1.await();
+                if (txCnt == 0) { coopRegisterThread(useCoopSched, 0); }
 
                 // generate optimistic mutation
+                sched(useCoopSched);
                 testMap1.put(1L, (long)txCnt);
-                if (txCnt % 2 == 0)
-                    testMap2.put(1L, (long)txCnt);
+                if (txCnt % 2 == 0) {
+                    sched(useCoopSched);
+                    testMap2.put(1L, (long) txCnt);
+                }
 
                 // wait for it to be undon
+                sched(useCoopSched);
                 TXEnd();
             }
             // signal done
             commitDone.set(true);
 
-            Assert.assertEquals((long)(txCnt-1), (long) testMap1.get(1L));
-            Assert.assertEquals((long)( (txCnt-1) % 2 == 0 ? (txCnt-1) : txCnt-2), (long) testMap2.get(1L));
+            String failMsg = useCoopSched ? ("schedule was: " + formatSchedule(schedule) + "\n") : "";
+            Assert.assertEquals(failMsg, (long)(txCnt-1), (long) testMap1.get(1L));
+            Assert.assertEquals(failMsg, (long)( (txCnt-1) % 2 == 0 ? (txCnt-1) : txCnt-2), (long) testMap2.get(1L));
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps affecting optimistic-rollback of the above thread
@@ -212,27 +309,67 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
 
             // signal that transaction has started and obtained a snapshot
             l1.countDown();
+            coopRegisterThread(useCoopSched, 1);
 
             // keep accessing the snapshot, causing optimistic rollback
 
             while (!commitDone.get()){
+                sched(useCoopSched);
                 testMap1.put(1L, specialVal);
+                sched(useCoopSched);
                 testMap2.put(1L, specialVal);
             }
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps syncing with the tail of log
         scheduleConcurrently(t -> {
             // signal that thread has started
             l1.countDown();
+            coopRegisterThread(useCoopSched, 2);
 
             // keep updating the in-memory proxy from the log
             while (!commitDone.get() ){
+                sched(useCoopSched);
                 testMap1.get(1L);
+                sched(useCoopSched);
                 testMap2.get(1L);
             }
+            coopThreadDone(useCoopSched);
         });
 
+        Thread coop = coopSetScheduleAndRun(useCoopSched, NTHREADS, NTHREADS, schedule);
         executeScheduled(NTHREADS, PARAMETERS.TIMEOUT_NORMAL);
+        coopJoinScheduler(coop);
+    }
+
+    private static void coopRegisterThread(boolean useCoopSched, int thrId) {
+        if (useCoopSched) {
+            CoopScheduler.registerThread(thrId);
+        }
+    }
+
+    private static void coopThreadDone(boolean useCoopSched) {
+        if (useCoopSched) {
+            CoopScheduler.threadDone();
+        }
+    }
+
+    private static Thread coopSetScheduleAndRun(boolean useCoopSched, int maxThreads, int nThreads, int[] schedule) {
+        Thread coop;
+        if (useCoopSched) {
+            coop = new Thread(() -> CoopScheduler.runScheduler(nThreads));
+            CoopScheduler.reset(maxThreads);
+            CoopScheduler.setSchedule(schedule);
+            coop = new Thread(() -> CoopScheduler.runScheduler(nThreads));
+            coop.start();
+        } else {
+            coop = new Thread(() -> { /* Do nothing */} );
+        }
+        return coop;
+    }
+
+    private static void coopJoinScheduler(Thread thr) throws InterruptedException {
+        thr.join();
     }
 }
