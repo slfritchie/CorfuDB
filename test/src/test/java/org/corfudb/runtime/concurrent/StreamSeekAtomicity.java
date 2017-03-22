974c55e8adf076ee9fd4155b5bf60905bd75f237
Scott Lystig Fritchie <sfritchie@vmware.com>
2017-03-22 12:27:47 -0400

    Refactor coop sched use in StreamSeekAtomicity

--- test/src/test/java/org/corfudb/runtime/concurrent/StreamSeekAtomicity.java
+++ test/src/test/java/org/corfudb/runtime/concurrent/StreamSeekAtomicity.java
@@ -37,11 +37,29 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
      */
     @Test
     public void ckCommitAtomicity() throws Exception {
+        ckCommitAtomicity(false);
+    }
+
+    /**
+     * This test verifies commit atomicity against concurrent -read- activity,
+     * which constantly causes rollbacks and optimistic-rollbacks.
+     * Use the cooperative scheduler for thread interleaving control.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ckCommitAtomicityCoop() throws Exception {
+        ckCommitAtomicity(true);
+    }
+
+    public void ckCommitAtomicity(boolean useCoopSched) throws Exception {
         String mapName1 = "testMapA";
         Map<Long, Long> testMap1 = instantiateCorfuObject(SMRMap.class, mapName1);
         CountDownLatch l1 = new CountDownLatch(2);
         AtomicBoolean commitDone = new AtomicBoolean(false);
         final int NTHREADS = 3;
+        int[] schedule = makeSchedule(NTHREADS, numIterations / innerIterations);
+        // printSchedule(schedule);
 
         scheduleConcurrently(t -> {
 
@@ -52,17 +70,21 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
                 // on first iteration, wait for all to start;
                 // other iterations will proceed right away
                 l1.await();
+                if (txCnt == 0) { coopRegisterThread(useCoopSched); }
 
                 // generate optimistic mutation
+                sched();
                 testMap1.put(1L, (long)txCnt);
 
                 // wait for it to be undon
+                sched();
                 TXEnd();
             }
             // signal done
             commitDone.set(true);
 
             Assert.assertEquals((long)(txCnt-1), (long) testMap1.get(1L));
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps affecting optimistic-rollback of the above thread
@@ -72,26 +94,34 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
 
             // signal that transaction has started and obtained a snapshot
             l1.countDown();
+            coopRegisterThread(useCoopSched);
 
             // keep accessing the snapshot, causing optimistic rollback
 
             while (!commitDone.get()){
+                sched();
                 testMap1.get(1L);
             }
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps syncing with the tail of log
         scheduleConcurrently(t -> {
             // signal that thread has started
             l1.countDown();
+            coopRegisterThread(useCoopSched);
 
             // keep updating the in-memory proxy from the log
             while (!commitDone.get() ){
+                sched();
                 testMap1.get(1L);
             }
+            coopThreadDone(useCoopSched);
         });
 
+        Thread coop = coopSetScheduleAndRun(useCoopSched, NTHREADS, NTHREADS, schedule);
         executeScheduled(NTHREADS, PARAMETERS.TIMEOUT_NORMAL);
+        coopJoinScheduler(coop);
     }
 
     /**
@@ -103,6 +133,23 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
      */
     @Test
     public void ckCommitAtomicity2() throws Exception {
+        ckCommitAtomicity2(false);
+    }
+
+    /**
+     * This test is similar to above, but with multiple maps.
+     * It verifies commit atomicity against concurrent -read- activity,
+     * which constantly causes rollbacks and optimistic-rollbacks.
+     * Use the cooperative scheduler for thread interleaving control.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ckCommitAtomicity2Coop() throws Exception {
+        ckCommitAtomicity2(true);
+    }
+
+    public void ckCommitAtomicity2(boolean useCoopSched) throws Exception {
         String mapName1 = "testMapA";
         Map<Long, Long> testMap1 = instantiateCorfuObject(SMRMap.class, mapName1);
         String mapName2 = "testMapB";
@@ -111,6 +158,8 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
         CountDownLatch l1 = new CountDownLatch(2);
         AtomicBoolean commitDone = new AtomicBoolean(false);
         final int NTHREADS = 3;
+        int[] schedule = makeSchedule(NTHREADS, numIterations / innerIterations);
+        // printSchedule(schedule);
 
         scheduleConcurrently(t -> {
 
@@ -121,6 +170,7 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
                 // on first iteration, wait for all to start;
                 // other iterations will proceed right away
                 l1.await();
+                if (txCnt == 0) { coopRegisterThread(useCoopSched); }
 
                 // generate optimistic mutation
                 testMap1.put(1L, (long)txCnt);
@@ -135,6 +185,7 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
 
             Assert.assertEquals((long)(txCnt-1), (long) testMap1.get(1L));
             Assert.assertEquals((long)( (txCnt-1) % 2 == 0 ? (txCnt-1) : txCnt-2), (long) testMap2.get(1L));
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps affecting optimistic-rollback of the above thread
@@ -144,28 +195,38 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
 
             // signal that transaction has started and obtained a snapshot
             l1.countDown();
+            coopRegisterThread(useCoopSched);
 
             // keep accessing the snapshot, causing optimistic rollback
 
             while (!commitDone.get()){
+                sched();
                 testMap1.get(1L);
+                sched();
                 testMap2.get(1L);
             }
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps syncing with the tail of log
         scheduleConcurrently(t -> {
             // signal that thread has started
             l1.countDown();
+            coopRegisterThread(useCoopSched);
 
             // keep updating the in-memory proxy from the log
             while (!commitDone.get() ){
+                sched();
                 testMap1.get(1L);
+                sched();
                 testMap2.get(1L);
             }
+            coopThreadDone(useCoopSched);
         });
 
+        Thread coop = coopSetScheduleAndRun(useCoopSched, NTHREADS, NTHREADS, schedule);
         executeScheduled(NTHREADS, PARAMETERS.TIMEOUT_NORMAL);
+        coopJoinScheduler(coop);
     }
 
     /**
@@ -176,11 +237,24 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
     @Test
     public void ckCommitAtomicity3() throws Exception {
         for (int i = 0; i < innerIterations; i++) {
-            ckCommitAtomicity3Inner(i);
+            ckCommitAtomicity3(i, false);
         }
     }
 
-    public void ckCommitAtomicity3Inner(int iter) throws Exception {
+    /**
+     * This test is similar to above, but with concurrent -write- activity.
+     * Use the cooperative scheduler for thread interleaving control.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ckCommitAtomicity3Coop() throws Exception {
+        for (int i = 0; i < innerIterations; i++) {
+            ckCommitAtomicity3(i, true);
+        }
+    }
+
+    public void ckCommitAtomicity3(int iter, boolean useCoopSched) throws Exception {
         String mapName1 = "testMapA" + iter;
         Map<Long, Long> testMap1 = instantiateCorfuObject(SMRMap.class, mapName1);
         String mapName2 = "testMapB" + iter;
@@ -189,7 +263,6 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
         CountDownLatch l1 = new CountDownLatch(2);
         AtomicBoolean commitDone = new AtomicBoolean(false);
         final int NTHREADS = 3;
-
         int[] schedule = makeSchedule(NTHREADS, numIterations / innerIterations);
         // printSchedule(schedule);
 
@@ -201,23 +274,26 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
                 // on first iteration, wait for all to start;
                 // other iterations will proceed right away
                 l1.await();
-                if (txCnt == 0) { CoopScheduler.registerThread(); } // sched() attempts before now will have no effect
+                if (txCnt == 0) { coopRegisterThread(useCoopSched); }
 
                 // generate optimistic mutation
-                sched(); testMap1.put(1L, (long)txCnt);
+                sched();
+                testMap1.put(1L, (long)txCnt);
                 if (txCnt % 2 == 0) {
-                    sched(); testMap2.put(1L, (long) txCnt);
+                    sched();
+                    testMap2.put(1L, (long) txCnt);
                 }
 
                 // wait for it to be undon
-                sched(); TXEnd();
+                sched();
+                TXEnd();
             }
             // signal done
             commitDone.set(true);
 
             Assert.assertEquals((long)(txCnt-1), (long) testMap1.get(1L));
             Assert.assertEquals((long)( (txCnt-1) % 2 == 0 ? (txCnt-1) : txCnt-2), (long) testMap2.get(1L));
-            CoopScheduler.threadDone();
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps affecting optimistic-rollback of the above thread
@@ -229,36 +305,67 @@ public class StreamSeekAtomicity extends AbstractObjectTest  {
 
             // signal that transaction has started and obtained a snapshot
             l1.countDown();
-            CoopScheduler.registerThread(); // sched() attempts before now will have no effect
+            coopRegisterThread(useCoopSched);
 
             // keep accessing the snapshot, causing optimistic rollback
 
             while (!commitDone.get()){
-                sched(); testMap1.put(1L, specialVal);
-                sched(); testMap2.put(1L, specialVal);
+                sched();
+                testMap1.put(1L, specialVal);
+                sched();
+                testMap2.put(1L, specialVal);
             }
-            CoopScheduler.threadDone();
+            coopThreadDone(useCoopSched);
         });
 
         // thread that keeps syncing with the tail of log
         scheduleConcurrently(t -> {
             // signal that thread has started
             l1.countDown();
-            CoopScheduler.registerThread(); // sched() attempts before now will have no effect
+            coopRegisterThread(useCoopSched);
 
             // keep updating the in-memory proxy from the log
             while (!commitDone.get() ){
-                sched(); testMap1.get(1L);
-                sched(); testMap2.get(1L);
+                sched();
+                testMap1.get(1L);
+                sched();
+                testMap2.get(1L);
             }
-            CoopScheduler.threadDone();
+            coopThreadDone(useCoopSched);
         });
 
-        CoopScheduler.reset(NTHREADS);
-        CoopScheduler.setSchedule(schedule);
-        Thread coop = new Thread(() -> CoopScheduler.runScheduler(NTHREADS) );
-        coop.start();
+        Thread coop = coopSetScheduleAndRun(useCoopSched, NTHREADS, NTHREADS, schedule);
         executeScheduled(NTHREADS, PARAMETERS.TIMEOUT_NORMAL);
-        coop.join();
+        coopJoinScheduler(coop);
+    }
+
+    private static void coopRegisterThread(boolean useCoopSched) {
+        if (useCoopSched) {
+            CoopScheduler.registerThread();
+        }
+    }
+
+    private static void coopThreadDone(boolean useCoopSched) {
+        if (useCoopSched) {
+            CoopScheduler.threadDone();
+        }
+    }
+
+    private static Thread coopSetScheduleAndRun(boolean useCoopSched, int maxThreads, int nThreads, int[] schedule) {
+        Thread coop;
+        if (useCoopSched) {
+            coop = new Thread(() -> CoopScheduler.runScheduler(nThreads));
+            CoopScheduler.reset(maxThreads);
+            CoopScheduler.setSchedule(schedule);
+            coop = new Thread(() -> CoopScheduler.runScheduler(nThreads));
+            coop.start();
+        } else {
+            coop = new Thread(() -> { /* Do nothing */} );
+        }
+        return coop;
+    }
+
+    private static void coopJoinScheduler(Thread thr) throws InterruptedException {
+        thr.join();
     }
 }
