1b99c49f3524625bfc2f389af3b433caf93b6ab4
Scott Lystig Fritchie <sfritchie@vmware.com>
2017-05-04 18:17:30 -0700

    Create SNAPSHOT dir mirror of TRAVIS_BUILD_NUMBER-based dir

--- .travis_scripts/mvnrepo.sh
+++ .travis_scripts/mvnrepo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_BRANCH" != "master" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+##ORIG: if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]; then
         echo -e "Publishing maven repository..."
 
@@ -10,8 +11,26 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; th
         cp -R target/mvn-repo $HOME/mvn-repo-current
         for pkg in runtime annotations annotationProcessor; do
             cp -R $pkg/target/mvn-repo/* $HOME/mvn-repo-current
+            (
+                cd $HOME/mvn-repo-current/*/*/$pkg
+                SNAPSHOT_DIR=`echo $PROJECT_VERSION | sed s/-.*/-SNAPSHOT/`
+                echo DBG: PWD = $PWD
+                echo DBG: SNAPSHOT_DIR = $SNAPSHOT_DIR
+                rm -rf $SNAPSHOT_DIR
+                mkdir $SNAPSHOT_DIR
+                cp -pv $PROJECT_VERSION/* $SNAPSHOT_DIR
+                (
+                    cd $SNAPSHOT_DIR
+                    for file in *.jar* *.pom*; do
+                        mv -v $i `echo $i | sed 's/-[0-9]*/-SNAPSHOT/'`
+                    done
+                )
+                echo DBG: contents of $SNAPSHOT_DIR
+                ls -al .
+            )
         done
         cd $HOME
+        echo DBG: Exiting now with status 0 ; exit 0
         git config --global user.email "travis@travis-ci.org"
         git config --global user.name "travis-ci"
         git clone --quiet --branch=mvn-repo https://${GH_TOKEN}@github.com/CorfuDB/Corfu-Repos mvn-repo > /dev/null
