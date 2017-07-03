e0e28afd589813c336b06be09c7d89d21096faf5
Scott Lystig Fritchie <sfritchie@vmware.com>
2017-07-03 17:34:34 -0400

    Exploratory hack to try to understand Caffeine cache behavior
    The output of this thing ... suggests that the limit on total size
    as defined by the maximumWeight() of the cache appears to be managed
    as expected, though there can be some 1 second time delay before
    an over-size cache falls down under its weight limit.
    
    However, the cache behavior of evicting entries that have expired
    appears to be advisory only?  The 'invalidated' and 'skipped' count
    are quite odd, being nowhere near 90 or 73 or other size estimates.
    
        ckCommitAtomicity2                                          [PASS]
        ckCommitAtomicity3                                          [PASS]
        ckCommitAtomicity                                           [PASS]
        Cache size = 150
        Advance logical clock
        Cache size = 150
        Cache size = 170
        Cache size = 166
        Cache size = 91
        Cache size = 91
        Advance logical clock to 165
        Cache size = 100
        Cache size = 90
        Cache size = 90
        Cache size = 90
        before refresh, Cache size = 90
        before invalidate, Cache size = 90
        before invalidate, asMap size = 90
        before invalidate, asMap = 4,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,
        before invalidate, asMap WTF size = 90
        before invalidate, asMap WTF length B = 31
        after invalidate, asMap size = 73
        after invalidate, asMap = 166,167,168,169,170,171,172,173,174,175,176,177,178,179,
        invalidated = 17, skipped = 14
        Cache size = 73, stats = CacheStats{hitCount=207, missCount=183, loadSuccessCount=200, loadFailureCount=0, totalLoadTime=3438901, evictionCount=93, evictionWeight=732}
        Cache size = 73, stats = CacheStats{hitCount=207, missCount=183, loadSuccessCount=200, loadFailureCount=0, totalLoadTime=3438901, evictionCount=93, evictionWeight=732}
        Cache size = 73, stats = CacheStats{hitCount=207, missCount=183, loadSuccessCount=200, loadFailureCount=0, totalLoadTime=3438901, evictionCount=93, evictionWeight=732}
        Cache size = 73, stats = CacheStats{hitCount=207, missCount=183, loadSuccessCount=200, loadFailureCount=0, totalLoadTime=3438901, evictionCount=93, evictionWeight=732}
        Get to 200
        Cache size = 93
        Cache size = 91
        Cache size = 91
        Cache size = 91
        asMap WTF length C = 34
        Cache size = 91
        caffeineTest                                                [PASS]

--- test/src/test/java/org/corfudb/runtime/concurrent/StreamSeekAtomicityTest.java
+++ test/src/test/java/org/corfudb/runtime/concurrent/StreamSeekAtomicityTest.java
@@ -1,15 +1,30 @@
 package org.corfudb.runtime.concurrent;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.Expiry;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.github.benmanes.caffeine.cache.Ticker;
+import com.google.common.collect.Maps;
+
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import org.corfudb.runtime.CorfuRuntime;
 import org.corfudb.runtime.collections.SMRMap;
 import org.corfudb.runtime.object.transactions.AbstractTransactionsTest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.Nonnull;
 
 /**
  * Created by dmalkhi on 3/17/17.
@@ -235,4 +250,129 @@ public class StreamSeekAtomicityTest extends AbstractTransactionsTest {
 
         executeScheduled(NTHREADS, PARAMETERS.TIMEOUT_NORMAL);
     }
+
+    @Getter
+    private long logicalTime = -1;
+    // public long getLogicalTime() { System.err.printf("lt = %d\n", logicalTime); return logicalTime; }
+
+    private class logicalTicker implements Ticker {
+        @Override
+        public long read() { /* System.err.printf("t=%d,", logicalTime); */ return logicalTime; }
+    }
+
+    @Test
+    public void caffeineTest() {
+        CorfuRuntime runtime = getRuntime();
+        Ticker myTicker = new logicalTicker();
+        final long weight = 100*8;
+
+        final LoadingCache<Long, String> readCache = Caffeine.<Long, String>newBuilder()
+                .<Long, String>weigher((k, v) -> v.length())
+                .maximumWeight(weight)
+                .expireAfter(new Expiry<Long, String>() {
+                    public long expireAfterCreate(Long key, String graph, long currentTime) {
+                        // Use wall clock time, rather than nanotime, if from an external resource
+                        return key;
+                    }
+                    public long expireAfterUpdate(Long key, String graph,
+                                                  long currentTime, long currentDuration) {
+                        return currentDuration;
+                    }
+                    public long expireAfterRead(Long key, String graph,
+                                                long currentTime, long currentDuration) {
+                        return currentDuration;
+                    }
+                })
+                // .ticker(this::getLogicalTime) // Same behavior as .ticker(myTicker)
+                .ticker(myTicker)
+                .recordStats()
+                .build(new CacheLoader<Long, String>() {
+                    @Override
+                    public String load(Long value) throws Exception {
+                        return cacheFetch(value);
+                    }
+
+                    @Override
+                    public Map<Long, String> loadAll(Iterable<? extends Long> keys) throws Exception {
+                        return cacheFetch((Iterable<Long>) keys);
+                    }
+                });
+        for (long i = 0; i < 150; i++) {
+            String yo = readCache.get(i);
+            // System.err.printf("i[%d] = %s\n", i, yo);
+        }
+        System.err.printf("Cache size = %d\n", readCache.estimatedSize());
+        System.err.printf("Advance logical clock\n");
+        logicalTime = 145;
+        System.err.printf("Cache size = %d\n", readCache.estimatedSize());
+        final long some = 150;
+        for (int j = 0; j < 4; j++) {
+            for (long i = some; i < some + (20); i++) {
+                String yo = readCache.get(i);
+                yo = readCache.get(4L);
+                // System.err.printf("i[%d] = %s\n", i, yo);
+                // System.err.printf("i[1] = %s\n", 1, readCache.get(1L));
+            }
+            System.err.printf("Cache size = %d\n", readCache.estimatedSize());
+            try { Thread.sleep(1000); } catch (Exception e) {}
+        }
+        logicalTime = 165;
+        System.err.printf("Advance logical clock to %d\n", logicalTime);
+        for (int j = 0; j < 4; j++) {
+            for (long i = some+20; i < some + (20) + 10; i++) {
+                String yo = readCache.get(i);
+                yo = readCache.get(4L);
+                // System.err.printf("i[%d] = %s\n", i, yo);
+                // System.err.printf("i[1] = %s\n", 1, readCache.get(1L));
+            }
+            System.err.printf("Cache size = %d\n", readCache.estimatedSize());
+            try { Thread.sleep(1000); } catch (Exception e) {}
+        }
+        System.err.printf("before refresh, Cache size = %d\n", readCache.estimatedSize());
+        readCache.asMap().keySet().stream().forEach(addr -> {
+            if (addr <= logicalTime) { readCache.refresh(addr); }
+        });
+        System.err.printf("before invalidate, Cache size = %d\n", readCache.estimatedSize());
+        System.err.printf("before invalidate, asMap size = %d\n", readCache.asMap().keySet().size());
+        System.err.printf("before invalidate, asMap = "); readCache.asMap().keySet().stream().sorted().forEach(l -> System.err.printf("%d,", l)); System.err.printf("\n");
+        System.err.printf("before invalidate, asMap WTF size = %d\n", readCache.asMap().keySet().size());
+        System.err.printf("before invalidate, asMap WTF length B = %d\n", readCache.asMap().keySet().stream().sorted().toArray().length);
+        AtomicInteger invalidated = new AtomicInteger(0);
+        AtomicInteger skipped = new AtomicInteger(0);
+        readCache.asMap().keySet().stream().forEach(addr -> {
+            if (addr <= logicalTime) { readCache.invalidate(addr); invalidated.getAndIncrement(); }
+            else { skipped.getAndIncrement(); }
+        });
+        System.err.printf("after invalidate, asMap size = %d\n", readCache.asMap().keySet().size());
+        System.err.printf("after invalidate, asMap = "); readCache.asMap().keySet().stream().sorted().forEach(l -> System.err.printf("%d,", l)); System.err.printf("\n");
+        System.err.printf("invalidated = %d, skipped = %d\n", invalidated.get(), skipped.get());
+        for (int j = 0; j < 4; j++) {
+            System.err.printf("Cache size = %d, stats = %s\n", readCache.estimatedSize(), readCache.stats());
+        }
+        System.err.printf("Get to 200\n");
+        for (long i = 170; i < 200; i++) {
+            String yo = readCache.get(i);
+            // System.err.printf("i[%d] = %s\n", i, yo);
+        }
+        for (int j = 0; j < 4; j++) {
+            System.err.printf("Cache size = %d\n", readCache.estimatedSize());
+            try { Thread.sleep(1000); } catch (Exception e) {}
+        }
+        System.err.printf("asMap WTF length C = %d\n", readCache.asMap().keySet().stream().sorted().toArray().length);
+        System.err.printf("Cache size = %d\n", readCache.estimatedSize());
+    }
+
+    private @Nonnull String cacheFetch(long address) {
+        return "Hello " + address;
+    }
+    public @Nonnull Map<Long, String> cacheFetch(Iterable<Long> addresses) {
+        Map<Long,String> res = new HashMap<>();
+        Iterator<Long> iterator = addresses.iterator();
+        while(iterator.hasNext()){
+            Long i = iterator.next();
+            res.put(i, cacheFetch(i));
+        }
+        return res;
+    }
+
 }
