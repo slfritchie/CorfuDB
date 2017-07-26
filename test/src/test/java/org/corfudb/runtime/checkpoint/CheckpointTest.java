0b0dec7a20bf943e7f16135390c6136d44780774
Scott Lystig Fritchie <sfritchie@vmware.com>
2017-07-26 18:27:24 -0400

    Add Dahlia's 'not working correctly' test case

--- test/src/test/java/org/corfudb/runtime/checkpoint/CheckpointTest.java
+++ test/src/test/java/org/corfudb/runtime/checkpoint/CheckpointTest.java
@@ -608,4 +608,55 @@ public class CheckpointTest extends AbstractObjectTest {
         getRuntime().getObjectsView().TXEnd();
 
     }
+
+    @Test
+    public void testSuccessiveCheckpointTrim() throws Exception {
+        final int nCheckpoints = 2;
+        final long ckpointGap = 5;
+
+        Map<String, String> testMap = getDefaultRuntime().getObjectsView().build()
+                .setTypeToken(new TypeToken<SMRMap<String, String>>() {})
+                .setStreamName("test")
+                .open();
+
+        long checkpointAddress = -1;
+        // generate two successive checkpoints
+        for (int ckpoint = 0; ckpoint < nCheckpoints; ckpoint++) {
+            // Place 3 entries into the map
+            testMap.put("a", "a"+ckpoint);
+            testMap.put("b", "b"+ckpoint);
+            testMap.put("c", "c"+ckpoint);
+
+            // Insert a checkpoint
+            MultiCheckpointWriter mcw = new MultiCheckpointWriter();
+            mcw.addMap((SMRMap) testMap);
+            checkpointAddress = mcw.appendCheckpoints(getRuntime(), "author");
+        }
+
+        // Trim the log in between the checkpoints
+        getRuntime().getAddressSpaceView().prefixTrim(checkpointAddress - ckpointGap);
+        getRuntime().getAddressSpaceView().gc();
+        getRuntime().getAddressSpaceView().invalidateServerCaches();
+        getRuntime().getAddressSpaceView().invalidateClientCache();
+
+        // Ok, get a new view of the map
+        Map<String, String> newTestMap = getDefaultRuntime().getObjectsView().build()
+                .setTypeToken(new TypeToken<SMRMap<String, String>>() {})
+                .addOption(ObjectOpenOptions.NO_CACHE)
+                .setStreamName("test")
+                .open();
+
+        // try to get a snapshot inside the gap
+        getRuntime().getObjectsView()
+                .TXBuild()
+                .setType(TransactionType.SNAPSHOT)
+                // fails Dahlia's original:  .setSnapshot(checkpointAddress-1)
+                // passes Dahlia's original: .setSnapshot(checkpointAddress)
+                .setSnapshot(checkpointAddress-1)
+                .begin();
+
+        // Reading an entry from scratch should be ok
+        assertThat(newTestMap.get("a"))
+                .isEqualTo("a"+(nCheckpoints-1));
+    }
 }
